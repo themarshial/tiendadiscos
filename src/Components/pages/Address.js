10e5a9ff8bf32f1cbbeba7287b441baae13d9a83
humildementebueno <themarshial@gmail.com>
2022-12-10 22:45:33 -0400

    feat: agregando altura en el maps

--- src/Components/pages/Address.js
+++ src/Components/pages/Address.js
@@ -9,7 +9,7 @@ const Address = () => {
         <h1 className='my-5 text-center text-white text-2xl'>Celular: 548772548</h1>
       </div>
 
-      <img className='flex justify-center w-auto mx-32' src='https://imgsrv2.voi.id/HpO09gZIQxpPSpVKdPkkZwaQELHpXUDAgoUw26q-tiA/auto/1200/675/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy8xNTE4MjMvMjAyMjAzMzEwODA3LW1haW4uSlBH.jpg'>
+      <img className='flex justify-center h-72 w-auto mx-32' src='https://imgsrv2.voi.id/HpO09gZIQxpPSpVKdPkkZwaQELHpXUDAgoUw26q-tiA/auto/1200/675/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy8xNTE4MjMvMjAyMjAzMzEwODA3LW1haW4uSlBH.jpg'>
       </img>
       {/* <iframe width="100%" height="600" src="https://www.google.com/maps/place/Restaurante+Como+en+Casa/@-16.4993403,-68.1544794,18.12z/data=!4m5!3m4!1s0x915edf2ad4a02cb9:0x58eba2e957ca2c75!8m2!3d-16.4989325!4d-68.1541508" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0">
 
