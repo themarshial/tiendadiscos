482b0aee5cd184642d3a8079da868757d470f7bd
humildementebueno <themarshial@gmail.com>
2022-12-15 09:54:50 -0400

    feat: adicione colores, imagenes , algo de contexto

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -4,7 +4,7 @@ import Jumbotron from '../shared/Jumbotron'
 
 const Home = () => {
   return (
-    <div className='text-center'>
+    <div className='text-center  bg-red-600'>
       <Carrusel/>
       <Jumbotron/>
     </div>
--- src/Components/shared/Jumbotron.js
+++ src/Components/shared/Jumbotron.js
@@ -6,12 +6,12 @@ const Jumbotron = () => {
       <div class="mt-4 md:w-1/2 md:flex  md:bg-white rounded-lg">
         <div>
           <img class="rounded-lg md:w-full"
-            src="https://st.depositphotos.com/1915171/5078/v/600/depositphotos_50782941-stock-illustration-user-sign-icon-person-symbol.jpg"
+            src="https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&h=500&q=80"
             alt=""/>
         </div>
         <div class="relative mx-2 p-2 shadow-lg bg-white md:bg-transparent rounded-lg -mt-2">
-          <h2 class="text-2xl tracking-tighter font-semibold uppercase text-gray-900  md:text-lg">Agente Secreto</h2>
-          <p class="text-gray-700 leading-snug">Diseñador de memes, obtiene buenos resultados ya!.</p>
+          <h2 class="text-2xl tracking-tighter font-semibold uppercase text-gray-900  md:text-lg">Hamburguesa con Queso</h2>
+          <p class="text-gray-700 leading-snug">Hamburguesa con queso y aderezo chipotle.</p>
           <div class="text-sm mt-2 font-semibold text-gray-700">USD $15</div>
           <div class="text-xs mt-2 text-yellow-700">&starf;&starf;&starf;&starf;&star; /36 reseñas</div>
 
